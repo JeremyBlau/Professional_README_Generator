2fcdfd42041bf179e95a2d5b9f23da36e8a65711
jwblau2 <jeremy@abundantly.com>
2023-08-21 20:32:58 -0600

    added function to write README

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const fs = require('fs');
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`README generated as ${fileName}`);
+      }
+    });
+  }
 
 // TODO: Create a function to initialize app
 function init() {}
