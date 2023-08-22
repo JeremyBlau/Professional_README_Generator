920b5fa824f0d7f4cab9721ca1042d1bb0f03e0f
jwblau2 <jeremy@abundantly.com>
2023-08-21 20:34:09 -0600

    added function to initialize app

--- index.js
+++ index.js
@@ -16,7 +16,48 @@ function writeToFile(fileName, data) {
   }
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+      const answers = await inquirer.prompt(questions);
+  
+      const readmeContent = `
+  # ${answers.projectTitle}
+  
+  ## Description
+  ${answers.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${answers.installation}
+  
+  ## Usage
+  ${answers.usage}
+  
+  ## License
+  ${answers.license}
+  
+  ## Contributing
+  ${answers.contributing}
+  
+  ## Tests
+  ${answers.tests}
+  
+  ## Questions
+  For any questions, please contact me at ${answers.email}. You can also visit my GitHub profile at [https://github.com/${answers.username}](https://github.com/${answers.username}).
+  `;
+  
+      writeToFile('README.md', readmeContent);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 // Function call to initialize app
 init();
