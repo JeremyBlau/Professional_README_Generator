5cec9856f0c9895d7f47a0a7b5f2d40f58d90b8d
jwblau2 <jeremy@abundantly.com>
2023-08-21 20:45:00 -0600

    added js code

--- index.js
+++ index.js
@@ -1,63 +1,76 @@
 // TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
+const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create a function to write README file
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a brief description of your project:'
+    },
+    // Add more questions for other sections
+];
 
+// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, err => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`README generated as ${fileName}`);
-      }
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`README generated as ${fileName}`);
+        }
     });
-  }
+}
 
 // TODO: Create a function to initialize app
 async function init() {
     try {
-      const answers = await inquirer.prompt(questions);
-  
-      const readmeContent = `
-  # ${answers.projectTitle}
-  
-  ## Description
-  ${answers.description}
-  
-  ## Table of Contents
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [License](#license)
-  - [Contributing](#contributing)
-  - [Tests](#tests)
-  - [Questions](#questions)
-  
-  ## Installation
-  ${answers.installation}
-  
-  ## Usage
-  ${answers.usage}
-  
-  ## License
-  ${answers.license}
-  
-  ## Contributing
-  ${answers.contributing}
-  
-  ## Tests
-  ${answers.tests}
-  
-  ## Questions
-  For any questions, please contact me at ${answers.email}. You can also visit my GitHub profile at [https://github.com/${answers.username}](https://github.com/${answers.username}).
-  `;
-  
-      writeToFile('README.md', readmeContent);
+        const answers = await inquirer.prompt(questions);
+
+        const readmeContent = `
+# ${answers.projectTitle}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+${answers.license}
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For any questions, please contact me at ${answers.email}. 
+`;
+
+        writeToFile('README.md', readmeContent);
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
-  }
+}
 
 // Function call to initialize app
 init();
